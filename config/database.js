e9a865593ed35a3d7a07f8da099ba6345291dcd9
dean <hindrough@gmail.com>
2015-04-25 21:52:16 -0700

    heroku database update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	url : 'mongodb://localhost/falcon'
+	url : 'mongodb://heroku_app29238002:k945vkmrhhjh8rmrlinop0queb@ds035310.mongolab.com:35310/heroku_app29238002'
 
 	//comment 'mongodb://heroku_app29238002:k945vkmrhhjh8rmrlinop0queb@ds035310.mongolab.com:35310/heroku_app29238002'
 }
